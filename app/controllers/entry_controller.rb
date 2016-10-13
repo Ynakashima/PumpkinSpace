a196aa028e321e1e1a7a81193e3c971faa1241ee
Nicole Url <nicole.url.4@gmail.com>
2016-10-12 19:21:12 -0700

    did controller methods

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -1,2 +1,40 @@
-class EntryController < ApplicationController
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.create(entry_params)
+  end
+
+  def edit
+    @id = params[:id]
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    if @entry.update(entry_params)
+      redirect_to entry_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id]).destroy
+    redirect_to entries_path
+  end
+
+####### PARAMS #####
+private
+
+  def entry_params
+    params.require(:entry).permit(:title, :joke)
+  end
+
 end
